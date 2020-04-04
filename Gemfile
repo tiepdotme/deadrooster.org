9d395919fa11757fc52318c28cdfe6ee670dc3b5
Mick F <mick@mickf.net>
2020-04-04 22:07:01 +0200

    style: fix rubocop errors

--- Gemfile
+++ Gemfile
@@ -10,8 +10,11 @@ gem 'kids'
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
-  # As of version 3.0.12, jekyll-assets can't work with Jekyll 4 but the master branch can
-  gem 'jekyll-assets', git: 'https://github.com/envygeeks/jekyll-assets', branch: 'master'
+  # As of version 3.0.12, jekyll-assets can't work with Jekyll 4
+  # ... but the master branch can
+  gem 'jekyll-assets',
+      git: 'https://github.com/envygeeks/jekyll-assets',
+      branch: 'master'
   gem 'jekyll-feed', '~> 0.12'
 end
 
--- _plugins/youtube_tag.rb
+++ _plugins/youtube_tag.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 module Jekyll
+  # A tag handling {% youtube foo %}
+  # where foo is a YouTube video id
   class YouTubeTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
       @youtube_id = text.strip
     end
 
-    def render(context)
-      content = super
-      <<~EOD
+    def render(_context)
+      <<~RENDERED_HTML
         <iframe width="560" height="315" src="https://www.youtube.com/embed/#{@youtube_id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
-      EOD
+      RENDERED_HTML
     end
   end
 end
